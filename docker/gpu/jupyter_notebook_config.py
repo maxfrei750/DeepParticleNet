89de2f305839148325e6d86bba746830f3dade19
Max Frei <max.frei@uni-due.de>
2018-12-18 23:32:43 +0100

    Initial commit.

---
+++ docker/gpu/jupyter_notebook_config.py
@@ -0,0 +1,45 @@
+# Copyright 2015 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+import os
+from IPython.lib import passwd
+
+c = c  # pylint:disable=undefined-variable
+c.NotebookApp.ip = '*'
+c.NotebookApp.port = int(os.getenv('PORT', 8888))
+c.NotebookApp.open_browser = False
+
+# sets a password if PASSWORD is set in the environment
+if 'PASSWORD' in os.environ:
+  password = os.environ['PASSWORD']
+  if password:
+    c.NotebookApp.password = passwd(password)
+  else:
+    c.NotebookApp.password = ''
+    c.NotebookApp.token = ''
+  del os.environ['PASSWORD']
+
+# Reference: https://svds.com/jupyter-notebook-best-practices-for-data-science/
+import os
+from subprocess import check_call
+
+def post_save(model, os_path, contents_manager):
+    """post-save hook for converting notebooks to .py scripts"""
+    if model['type'] != 'notebook':
+        return # only do this for notebooks
+    d, fname = os.path.split(os_path)
+    check_call(['jupyter', 'nbconvert', '--to', 'script', fname], cwd=d)
+    #check_call(['jupyter', 'nbconvert', '--to', 'html', fname], cwd=d)
+
+c.FileContentsManager.post_save_hook = post_save
