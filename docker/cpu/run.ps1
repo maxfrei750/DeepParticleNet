00fde93b4b0156723bfe50a6bd61d89f08f631d0
Max Frei <max.frei@uni-due.de>
2018-12-23 21:20:18 +0100

    Clean up script with line continuations.

--- docker/cpu/run.ps1
+++ docker/cpu/run.ps1
@@ -1,10 +1,27 @@
 $basePath = Join-Path $PSScriptRoot "/../.."
 
 # Run container.
-docker run -d --rm --name deepparticlenet --network=bridge -p 8888:8888 -p 6006:6006 -e PASSWORD=$env:UserName --rm -v `"$basePath`":/notebooks maxfrei750/deepparticlenet:tf1.10.0-cpu-py3
+docker run `
+    --rm `
+    --detach `
+    --name deepparticlenet `
+    --network=bridge `
+    --publish 8888:8888 `
+    --publish 6006:6006 `
+    --env PASSWORD=$env:UserName `
+    --volume `"$basePath`":/notebooks `
+    maxfrei750/deepparticlenet:tf1.10.0-cpu-py3
 
 # Run tensorboard.
-docker exec -d deepparticlenet tensorboard --logdir=/notebooks/logs
+docker exec `
+    --detach `
+    deepparticlenet `
+    tensorboard --logdir=/notebooks/logs
 
 # Open a terminal.
-docker exec -it deepparticlenet bash
+docker exec `
+    --interactive `
+    --tty `
+    deepparticlenet `
+    bash   
+    
