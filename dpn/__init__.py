2e6f1490ee23f913ad9f08a693d4c58d97b039e8
Max Frei <max.frei@uni-due.de>
2018-12-26 23:17:30 +0100

    Add root dir of the dpn module to the python path.

--- dpn/__init__.py
+++ dpn/__init__.py
@@ -1,6 +1,10 @@
-# Add the path of Mask_RCNN to use it as a submodule.
 import os
 import sys
 
+# Add the path of Mask_RCNN to use it as a submodule.
+DPN_DIR = os.path.abspath(os.path.join(__path__[0], ".."))
+sys.path.append(DPN_DIR)
+
+# Add the path of Mask_RCNN to use it as a submodule.
 MRCNN_DIR = os.path.abspath(os.path.join(__path__[0], "..", "external", "Mask_RCNN"))
 sys.path.append(MRCNN_DIR)
