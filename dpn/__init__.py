1b8324ed8194ff0602f78f0d74b0c0c1e3fc0366
Max Frei <max.frei@uni-due.de>
2018-12-27 00:03:12 +0100

    Check if the paths are already on the search path.

--- dpn/__init__.py
+++ dpn/__init__.py
@@ -3,8 +3,10 @@ import sys
 
 # Add the path of Mask_RCNN to use it as a submodule.
 DPN_DIR = os.path.abspath(os.path.join(__path__[0], ".."))
-sys.path.append(DPN_DIR)
+if DPN_DIR not in sys.path:  # Check if the path is already on the search path.
+    sys.path.append(DPN_DIR)
 
 # Add the path of Mask_RCNN to use it as a submodule.
 MRCNN_DIR = os.path.abspath(os.path.join(__path__[0], "..", "external", "Mask_RCNN"))
-sys.path.append(MRCNN_DIR)
+if MRCNN_DIR not in sys.path:  # Check if the path is already on the search path.
+    sys.path.append(MRCNN_DIR)
