85473f89f2838758381f7554057a55a6ee078fc5
Max Frei <max.frei@uni-due.de>
2019-02-11 15:09:12 +0100

    Reimplemented function to analyze a complete dataset.

--- dpn/model.py
+++ dpn/model.py
@@ -1,10 +1,5 @@
-import numpy as np
-
 from mrcnn.model import MaskRCNN
-from dpn.sizedistribution import SizeDistribution
-from dpn.utilities import *
-
-from skimage.segmentation import clear_border
+from dpn.results import Results
 
 
 class Model(MaskRCNN):
@@ -21,50 +16,19 @@ class Model(MaskRCNN):
                                  custom_callbacks=self.config.CUSTOM_CALLBACKS,
                                  no_augmentation_sources=self.config.NO_AUGMENTATION_SOURCES)
 
-    def analyze_dataset(self, dataset, mode="mask", filter_border_objects=False):
-
-        mode = mode.lower()
-
-        # Check inputs.
-        assert mode in ["mask", "bbox"], "Expected mode to be \"mask\" or \"bbox\"."
-
-        # Initialize diameters array.
-        diameters = np.uint32([])
+    def analyze_dataset(self, dataset):
+        # Create a Results-object.
+        results = Results()
 
         for image_id in dataset.image_ids:
+
+            # Load image.
             image = dataset.load_image(image_id)
 
-            # Run object detection
-            results = self.detect([image])
-            results = results[0]
+            # Perform detection.
+            new_results_dict = self.detect(image)
 
-            # Extract masks and bounding boxes.
-            masks = results["masks"]
-            bboxes = results["rois"]
+            # Append results.
+            results.append_from_dicts(new_results_dict)
 
-            if filter_border_objects:
-                # Filter objects that touch the image border.
-                cleared_masks = clear_border(masks)
-                do_keep = np.any(cleared_masks, (0, 1))
-
-                masks = masks[:, :, do_keep]
-                bboxes = bboxes[do_keep, :]
-
-            if mode == "mask":
-                # Calculate areas as the sum of the pixels of the masks.
-                areas = masks.sum(axis=(0, 1))
-
-                # Calculate new diameters
-                diameters_new = calculate_equivalent_diameter(areas)
-            else:  # elif mode == "bbox":
-                # Keep only the largest bbox dimension.
-                diameters_new = get_largest_bbox_dimension(bboxes)
-
-            # Concatenate diameters_new to the diameters array.
-            diameters = np.concatenate((diameters, diameters_new))
-
-        # Create and return a SizeDistribution-object.
-        psd = SizeDistribution("px")
-        psd.diameters = diameters
-
-        return psd
+        return results
