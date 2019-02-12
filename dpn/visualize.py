c65c5c130d5ee24fd567d9f3b5a200deef404272
Max Frei <max.frei@uni-due.de>
2019-02-12 11:19:00 +0100

    Added function to plot particle outlines.

--- dpn/visualize.py
+++ dpn/visualize.py
@@ -1,6 +1,8 @@
 from mrcnn.visualize import *
-
 import matplotlib.pyplot as plt
+from matplotlib.patches import Polygon
+from skimage.measure import find_contours
+from scipy.ndimage import binary_fill_holes
 
 
 def display_image(image, title="", figsize=(16, 16), ax=None):
@@ -37,3 +39,65 @@ def get_axis(rows=1, cols=1, size=32):
     """
     _, ax = plt.subplots(rows, cols, figsize=(size * cols, size * rows))
     return ax
+
+
+def display_instance_outlines(image, masks,
+                              figsize=(16, 16),
+                              ax=None,
+                              colors=None,
+                              linewidth=4,
+                              alpha=1,
+                              linestyle="-"):
+    """
+    masks: [height, width, num_instances]
+    figsize: (optional) the size of the image
+    colors: (optional) An array or colors to use with each object
+    """
+    # Number of instances
+    N = masks.shape[2]
+    if not N:
+        print("\n*** No instances to display *** \n")
+
+    # If no axis is passed, create one and automatically call show()
+    auto_show = False
+    if not ax:
+        _, ax = plt.subplots(1, figsize=figsize)
+        auto_show = True
+
+    # Generate random colors
+    colors = colors or random_colors(N)
+
+    # Show area outside image boundaries.
+    height, width = image.shape[:2]
+    ax.set_ylim(height + 10, -10)
+    ax.set_xlim(-10, width + 10)
+    ax.axis('off')
+
+    masked_image = image.astype(np.uint32).copy()
+    for i in range(N):
+        color = colors[i]
+
+        # Mask
+        mask = masks[:, :, i]
+
+        mask = binary_fill_holes(mask)
+
+        # Mask Polygon
+        # Pad to ensure proper polygons for masks that touch image edges.
+        padded_mask = np.zeros(
+            (mask.shape[0] + 2, mask.shape[1] + 2), dtype=np.uint8)
+        padded_mask[1:-1, 1:-1] = mask
+        contours = find_contours(padded_mask, 0.5)
+        for verts in contours:
+            # Subtract the padding and flip (y, x) to (x, y)
+            verts = np.fliplr(verts) - 1
+            p = Polygon(verts,
+                        facecolor="none",
+                        edgecolor=color,
+                        linewidth=linewidth,
+                        alpha=alpha,
+                        linestyle=linestyle)
+            ax.add_patch(p)
+    ax.imshow(masked_image.astype(np.uint8))
+    if auto_show:
+        plt.show()
