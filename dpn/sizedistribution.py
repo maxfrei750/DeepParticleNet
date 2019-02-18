ca2c77e125c1e232caad3b37a63abd99d6a48dd6
Max Frei <max.frei@uni-due.de>
2019-02-18 10:13:27 +0100

    Added function to compare size distributions.

--- dpn/sizedistribution.py
+++ dpn/sizedistribution.py
@@ -46,3 +46,34 @@ class SizeDistribution(object):
     def to_pixel(self, scalingfactor_meterperpixel):
         self.sizes = self.sizes / scalingfactor_meterperpixel
         self.unit = "px"
+
+    def compare(self, ground_truth, return_errors=False):
+        # Assert that the size distributions have the same unit.
+        assert self.unit == ground_truth.unit, "You cannot concatenate sizedistributions with different units."
+
+        d_g = self.geometric_mean
+        s_g = self.geometric_standard_deviation
+        N = self.number_of_particles
+
+        d_g_gt = ground_truth.geometric_mean
+        s_g_gt = ground_truth.geometric_standard_deviation
+        N_gt = ground_truth.number_of_particles
+
+        error_d_g = d_g / d_g_gt
+        error_s_g = s_g / s_g_gt
+        error_N = N / N_gt
+
+        print("d_g = {:.0f}".format(d_g))
+        print("d_g_gt = {:.1f}".format(d_g_gt))
+        print("error_d_g = {:.3f}".format(error_d_g))
+        print("\n")
+        print("s_g = {:.2f}".format(s_g))
+        print("s_g_gt = {:.2f}".format(s_g_gt))
+        print("error_s_g = {:.3f}".format(error_s_g))
+        print("\n")
+        print("N = {:.0f}".format(N))
+        print("N_gt = {:.0f}".format(N_gt))
+        print("error_N = {:.3f}".format(error_N))
+
+        if return_errors:
+            return error_d_g, error_s_g, error_N
