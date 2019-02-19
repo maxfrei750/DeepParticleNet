5709035c3d14d4da12081bf8d91526bc7a874e2f
Max Frei <max.frei@uni-due.de>
2019-02-19 10:06:16 +0100

    Make use of the Detection class.

--- dpn/model.py
+++ dpn/model.py
@@ -1,5 +1,7 @@
 from mrcnn.model import MaskRCNN
 from dpn.results import Results
+from dpn.detection import Detection
+import numpy as np
 
 
 class Model(MaskRCNN):
@@ -16,6 +18,35 @@ class Model(MaskRCNN):
                                  custom_callbacks=self.config.CUSTOM_CALLBACKS,
                                  no_augmentation_sources=self.config.NO_AUGMENTATION_SOURCES)
 
+    def detect(self, image, verbose=0):
+        # Call the training method of the super class.
+        results_dict_list = super(Model, self).detect([image], verbose=verbose)
+        results_dict = results_dict_list[0]
+
+        # Extract properties from results_dict.
+        masks = results_dict["masks"]
+        class_ids = results_dict["class_ids"]
+        bboxes = results_dict["rois"]
+        scores = results_dict["scores"]
+
+        # Convert class_ids to list.
+        class_ids = class_ids.tolist()
+
+        # Convert bboxes to list.
+        bboxes = bboxes.tolist()
+
+        # Convert scores to list.
+        scores = scores.tolist()
+
+        # Get number of instances
+        number_of_instances = len(class_ids)
+
+        # Convert masks to list of masks.
+        masks = np.split(masks, number_of_instances, axis=2)
+        masks = [np.squeeze(mask) for mask in masks]
+
+        return Detection(image, masks, class_ids, bboxes, scores)
+
     def analyze_dataset(self, dataset):
         # Create a Results-object.
         results = Results()
@@ -26,9 +57,9 @@ class Model(MaskRCNN):
             image = dataset.load_image(image_id)
 
             # Perform detection.
-            new_results_dict = self.detect([image])
+            new_detection = self.detect(image)
 
             # Append results.
-            results.append_from_dicts(new_results_dict)
+            results.append_detection(new_detection)
 
         return results
