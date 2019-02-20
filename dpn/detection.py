c3d44cb103fcc99a59781969f7f77aea76a2a16d
Max Frei <max.frei@uni-due.de>
2019-02-20 11:43:09 +0100

    Added property: areas

--- dpn/detection.py
+++ dpn/detection.py
@@ -2,6 +2,7 @@ from dpn.visualize import display_instance_outlines
 import numpy as np
 from itertools import compress
 from skimage.segmentation import clear_border
+from skimage.measure import perimeter
 import matplotlib.pyplot as plt
 
 
@@ -13,6 +14,12 @@ class Detection:
         self.bboxes = bboxes
         self.scores = scores
 
+    # Dependant attributes
+    @property
+    def areas(self):
+        return [np.sum(mask) for mask in self.masks]
+
+    # Methods
     def display_detection_image(self,
                                 do_return_figure_handle=False,
                                 linewidth=1.5,
@@ -64,6 +71,12 @@ class Detection:
 
     def filter_by_maximum_area(self, maximum_area):
         # Remove instances with areas larger than the given maximum area.
+        areas = [np.sum(mask) for mask in self.masks]
+        do_keep = [area <= maximum_area for area in areas]
+        self.filter_by_list(do_keep)
+
+    def filter_by_minimum_circularity(self, minimum_circularity):
+        # Remove instances with circularities smaller than the given minimum circularity.
         areas = [np.sum(mask) for mask in self.masks]
         do_keep = [area <= maximum_area for area in areas]
         self.filter_by_list(do_keep)
