467b5282d0af46c9758f0539a14b6ca229e40e21
Max Frei <max.frei@uni-due.de>
2019-02-27 14:36:30 +0100

    Automatically start tensorboard.

--- docker/cpu/run.sh
+++ docker/cpu/run.sh
@@ -5,10 +5,17 @@ basePath=$( dirname "$temp" )
 
 docker run \
 	--rm \
-	-it \
+	--interactive \
+	--detach
 	--name deepparticlenet \
 	--network=host \
 	--volume "$basePath":/tf \
 	--volume /media/data_fast/datasets/:/tf/datasets/ \
 	--volume /media/data_fast/logs/:/tf/logs/ \
 	maxfrei750/deepparticlenet:cpu
+
+# Start tensorboard.
+docker exec --detach deepparticlenet tensorboard --logdir=/tf/logs
+
+# Reattach to the docker container.
+docker attach deepparticlenet
--- docker/gpu/run.sh
+++ docker/gpu/run.sh
@@ -3,12 +3,20 @@
 temp=$( realpath "$0"  )
 basePath=$( dirname "$temp" )
 
-nvidia-docker run \
+docker run \
+	--runtime=nvidia \
 	--rm \
-	-it \
+	--interactive \
+	--detach
 	--name deepparticlenet \
 	--network=host \
 	--volume "$basePath":/tf \
 	--volume /media/data_fast/datasets/:/tf/datasets/ \
 	--volume /media/data_fast/logs/:/tf/logs/ \
 	maxfrei750/deepparticlenet:gpu
+
+# Start tensorboard.
+docker exec --detach deepparticlenet tensorboard --logdir=/tf/logs
+
+# Reattach to the docker container.
+docker attach deepparticlenet
