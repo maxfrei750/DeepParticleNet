25302f6c28aa455b57dcae42e1b5d3f98684f27c
Max Frei <max.frei@uni-due.de>
2019-07-05 15:24:11 +0200

    Improved documentation.

--- dpn/storable.py
+++ dpn/storable.py
@@ -2,12 +2,25 @@ import dill
 
 
 class Storable:
+    """Abstract class for storable objects"""
+
     def save(self, output_path):
+        """Save an object to a file.
+
+        :param output_path: Path of the output file.
+        :return: nothing
+        """
         with open(output_path, "wb") as file:
             dill.dump(self, file)
 
     @staticmethod
     def load(input_path):
+        """Load an object from a file.
+
+        :param input_path: Path of the input file.
+        :return: nothing
+        """
+
         dill._dill._reverse_typemap['ClassType'] = type
         with open(input_path, "rb") as file:
             return dill.load(file)
